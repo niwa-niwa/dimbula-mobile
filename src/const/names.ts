7f75748c9b9f8b80ec384cc6db4b5f64b603bfcf
niwa-niwa <urban.hang274@gmail.com>
2021-07-13 00:25:51 +0900

    added const file

---
+++ src/const/names.ts
@@ -0,0 +1,11 @@
+export const NAMES = {
+  TITLE: "Dimbula",
+  STORAGE_UID: "uid",
+  STORAGE_TOKEN: "token",
+  STORAGE_REFRESH_TOKEN: "refresh_token",
+  STORAGE_PASSWORD: "password",
+  STORAGE_EMAIL: "email",
+  STORAGE_REDIRECT: "redirect",
+  V1 : "api/v1/",
+  REFRESH_URL : "https://securetoken.googleapis.com/v1/token?key=",
+};
