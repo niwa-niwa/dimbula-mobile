e10ec436c65fb46621a39cd3d8dd8ea731097c6c
niwa-niwa <urban.hang274@gmail.com>
2021-07-14 00:07:38 +0900

    implemented email sign in

--- src/sign/SignIn.tsx
+++ src/sign/SignIn.tsx
@@ -1,33 +1,60 @@
 import React from "react";
-import { View, Text,} from "react-native";
-import { Button } from 'react-native-paper';
-import styled from 'styled-components/native';
+import { View, Text } from "react-native";
+import { Button } from "react-native-paper";
+import styled from "styled-components/native";
 import { Center } from "../layouts/Center";
+import firebase from "../apis/firebase";
+import "firebase/auth";
+import { NAMES } from "../const/names";
 
-import { NAMES } from '../const/names';
-
-
-interface SignInProps {};
+interface SignInProps {}
 export const SignIn: React.FC<SignInProps> = ({}) => {
-  const onPressGoogle = ():void=>{
-    console.log("onPress Google");
-  }
+
+  const onPressGoogle = (): void => {
+    const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(googleAuthProvider)
+      .then(() => {
+        console.log("Success sign in with google")
+      })
+      .catch((e) => {
+        console.log("Fail sign in with google",e)
+      });
+  };
+
+  const onPressEmail = (): void => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(
+        "pass_mail39-magazine@yahoo.co.jp",
+        "t4169N5175"
+      )
+      .then((response: any) => {
+        console.log(response);
+      })
+      .catch((e: any) => {
+        console.log(e);
+      });
+  };
 
   return (
     <Center>
       <H1_Text>{NAMES.TITLE}</H1_Text>
       <Wrapper>
-        <Text>Sign In</Text>
-        <Button 
-          onPress={onPressGoogle}
-          mode="outlined"
-        >
+        <Text>Sign In With Google</Text>
+        <Button onPress={onPressGoogle} mode="outlined">
           Sign In With Google
+        </Button>
+
+        <Text>Sign In With Email</Text>
+        <Button onPress={onPressEmail} mode="outlined">
+          Sign In With Email
         </Button>
       </Wrapper>
     </Center>
-  )
-  
+  );
+
 };
 
 const H1_Text = styled.Text`
@@ -39,7 +66,7 @@ const Wrapper = styled.View`
   display: flex;
   flex-flow: column;
   border: solid 2px #cccccc;
-  border-radius: 16px ;
-  max-width:320px;
-  width:70%;
+  border-radius: 16px;
+  max-width: 320px;
+  width: 70%;
 `;
