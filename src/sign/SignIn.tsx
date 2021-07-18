69c569885d9639473e2233c1dbdb75df86ee6bf5
niwa-niwa <urban.hang274@gmail.com>
2021-07-18 23:59:26 +0900

    instalee

--- src/sign/SignIn.tsx
+++ src/sign/SignIn.tsx
@@ -1,23 +1,29 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { Button } from "react-native-paper";
+import { Text, View, TextInput, Button, Alert, StyleSheet } from "react-native";
+import { Button as PaperButton } from 'react-native-paper';
 import styled from "styled-components/native";
 import { Center } from "../layouts/Center";
 import firebase from "../apis/firebase";
 import "firebase/auth";
 import { NAMES } from "../const/names";
+import { useForm, Controller } from "react-hook-form";
 
 // TODO : implement SignIn.tsx without google auth
 // TODO : implement SignUp.tsx without google auth
 // TODO : implement ResendEmail.tsx without google auth
 // TODO : implement ForgetPw.tsx without google auth
 
-interface SignInProps {}
+
+
+interface SignInProps {};
 export const SignIn: React.FC<SignInProps> = ({}) => {
+  const { control, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = ( data:any ) => console.log(data);
+
 
   React.useEffect(()=>{
     firebase.auth().onAuthStateChanged(async (user)=>{
-        console.log(user);
+        // console.log(user);
     });
   },[]);
 
@@ -39,12 +45,44 @@ export const SignIn: React.FC<SignInProps> = ({}) => {
   return (
     <Center>
       <H1_Text>{NAMES.TITLE}</H1_Text>
-      <Wrapper>
-        <Text>Sign In With Email</Text>
-        <Button onPress={onPressEmail} mode="outlined">
+        {/* <PaperButton onPress={onPressEmail} mode="outlined">
           Sign In With Email
-        </Button>
-      </Wrapper>
+        </PaperButton> */}
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              style={styles.input}
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
+          name="firstName"
+          defaultValue=""
+        />
+        {errors.firstName && <Text>This is required.</Text>}
+        <Controller
+          control={control}
+          rules={{
+            maxLength: 100,
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              style={styles.input}
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
+          name="lastName"
+          defaultValue=""
+        />
+
+      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
     </Center>
   );
 
@@ -55,11 +93,14 @@ const H1_Text = styled.Text`
   font-weight: bold;
 `;
 
-const Wrapper = styled.View`
-  display: flex;
-  flex-flow: column;
-  border: solid 2px #cccccc;
-  border-radius: 16px;
-  max-width: 320px;
-  width: 70%;
-`;
+interface Styles{
+  input:object
+};
+const styles= StyleSheet.create<Styles>({
+  input:{
+    borderStyle:"solid",
+    width:100,
+    height:32,
+    backgroundColor:"green"
+  }
+})
