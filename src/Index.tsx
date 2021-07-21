b975a02a75272f5fa552460ea3d2e5f68d9a9d49
niwa-niwa <urban.hang274@gmail.com>
2021-07-21 23:17:54 +0900

    styled ResendEmail

--- src/Index.tsx
+++ src/Index.tsx
@@ -4,7 +4,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { SignIn } from "./sign/SignIn";
 import { SignUp } from "./sign/SignUp";
-import ResendEmail from "./sign/ResendEmail";
+import { ResendEmail } from "./sign/ResendEmail";
 import { ForgetPw } from "./sign/ForgetPw";
 import { TaskFolder } from "./task/TaskFolder";
 import { TaskList } from "./task/TaskList";
